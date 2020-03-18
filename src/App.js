dc04ba51b8c6ba7e711500eb13522f2c739b62d0
chibuisi <amiaka.chibuisi@gmail.com>
2020-03-18 16:41:18 -0500

    published designer jd

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Footer from "./my_comps/Footer";
+import NavigationBar from "./my_comps/NavigationBar";
+import Welcome from "./my_comps/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Coming Soon
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Chibuisi.com
-        </a>
-      </header>
+        <NavigationBar/>
+        <Welcome/>
       <Footer/>
     </div>
   );
